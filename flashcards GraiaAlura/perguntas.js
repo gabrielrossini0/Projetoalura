4f9ac724382b0ca192efef493a127d4e37e2d7da
gabrielrossini0 <graiagabriel0@gmail.com>
2024-11-19 13:54:09 -0300

    Projeto 2b alura
    graia 2 b

---
+++ flashcards GraiaAlura/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript ?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'Programação',
+    'O que é o VS Code?',
+    'É uma plataforma com cursos de programação visual'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz como vai voçe? em Inglês?',
+    'Como vai voçe? em ingles é how are you? (RAI)'
+)
